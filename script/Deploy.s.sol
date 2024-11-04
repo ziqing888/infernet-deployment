1df4cb03f712246f5c51f59e736ab5ebcfeb942e
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-04 14:35:13 +0800

    Update Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,20 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import "../src/OfficialContract.sol";
 
 contract Deploy is Script {
+    address registryAddress = 0xe2F36C4E23D67F81fE0B278E80ee85Cf0ccA3c8d; // 从 config.json 中的地址
+
     function run() external {
         vm.startBroadcast();
         
-        // 部署官方合约
-        OfficialContract officialContract = new OfficialContract();
+        // 使用已经存在的合约地址
+        // 这里假设你已经知道该合约的接口
+        OfficialContract officialContract = OfficialContract(registryAddress);
         
-        // 输出合约地址，写入到文件中
+        // 输出合约地址
         console.log("OfficialContract 部署地址:", address(officialContract));
         
-        // 将合约地址写入到文件
-        vm.writeFile("deployed_address.txt", abi.encodePacked(address(officialContract)));
+        // 如果需要将地址写入文件
+        vm.writeFile("deployed_address.txt", abi.encodePacked(registryAddress));
 
         vm.stopBroadcast();
     }
