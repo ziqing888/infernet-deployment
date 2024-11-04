623394c8fa841fc8e6af129a507c95607dece4fb
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-04 08:33:01 +0800

    Update CallContract.s.sol

--- script/CallContract.s.sol
+++ script/CallContract.s.sol
@@ -3,20 +3,21 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 
-interface IMyContract {
-    function sayHello() external;
+interface IOfficialContract {
+    function officialMethod() external;
 }
 
 contract CallContract is Script {
     function run() external {
-        // 指定合约地址
-        address myContractAddress = 0xYourContractAddress;
+        // 指定 OfficialContract 的部署地址
+        address officialContractAddress = 0xYourContractAddress;  // 替换为实际部署地址
 
         vm.startBroadcast();
 
-        // 调用合约方法
-        IMyContract(myContractAddress).sayHello();
+        // 调用官方合约方法
+        IOfficialContract(officialContractAddress).officialMethod();
 
         vm.stopBroadcast();
     }
 }
+
