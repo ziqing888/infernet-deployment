e9df1283ea43f7ee956b523b0e95d28630b95c97
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-04 08:32:21 +0800

    Update Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,15 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import "../src/MyContract.sol";
+import "../src/OfficialContract.sol";
 
 contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
         
-        // 部署合约
-        MyContract myContract = new MyContract();
+        // 部署官方合约
+        OfficialContract officialContract = new OfficialContract();
         
+        // 输出合约地址，方便后续调用
+        console.log("OfficialContract 部署地址:", address(officialContract));
+
         vm.stopBroadcast();
     }
 }
