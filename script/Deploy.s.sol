b1605ca8f97b19cf3ac9be87c01ba8f280950407
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-04 14:48:14 +0800

    Update Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,7 +1,9 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
+import {SaysGM} from "../src/SaysGM.sol"; // 引入 SaysGM 合约
 
 contract Deploy is Script {
     address registryAddress = 0xe2F36C4E23D67F81fE0B278E80ee85Cf0ccA3c8d; // 从 config.json 中的地址
@@ -9,17 +11,15 @@ contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
         
-        // 使用已经存在的合约地址
-        // 这里假设你已经知道该合约的接口
-        OfficialContract officialContract = OfficialContract(registryAddress);
+        // 部署 SaysGM 合约
+        SaysGM saysGMContract = new SaysGM(registryAddress);
         
-        // 输出合约地址
-        console.log("OfficialContract 部署地址:", address(officialContract));
+        // 输出 SaysGM 合约地址
+        console.log("SaysGM 合约已部署，地址为:", address(saysGMContract));
         
-        // 如果需要将地址写入文件
-        vm.writeFile("deployed_address.txt", abi.encodePacked(registryAddress));
+        // 如果需要将合约地址写入文件
+        vm.writeFile("deployed_address.txt", abi.encodePacked(address(saysGMContract)));
 
         vm.stopBroadcast();
     }
 }
-
